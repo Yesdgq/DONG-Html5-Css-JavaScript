2604acb476601204472e17c0a042629c3e39e466
董国庆 <yesdgq@yahoo.com>
2017-07-24 15:42:01 +0800

    first commit

---
+++ DONG-Html5-Css-JavaScriptTests/DONG_Html5_Css_JavaScriptTests.m
@@ -0,0 +1,39 @@
+//
+//  DONG_Html5_Css_JavaScriptTests.m
+//  DONG-Html5-Css-JavaScriptTests
+//
+//  Created by yesdgq on 2017/7/24.
+//  Copyright © 2017年 yesdgq. All rights reserved.
+//
+
+#import <XCTest/XCTest.h>
+
+@interface DONG_Html5_Css_JavaScriptTests : XCTestCase
+
+@end
+
+@implementation DONG_Html5_Css_JavaScriptTests
+
+- (void)setUp {
+    [super setUp];
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown {
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    [super tearDown];
+}
+
+- (void)testExample {
+    // This is an example of a functional test case.
+    // Use XCTAssert and related functions to verify your tests produce the correct results.
+}
+
+- (void)testPerformanceExample {
+    // This is an example of a performance test case.
+    [self measureBlock:^{
+        // Put the code you want to measure the time of here.
+    }];
+}
+
+@end
